ccf677510aff7e093a09a4c0581583aa9a939575
haifei.zhang <haifei.zhang@chaitin.com>
2019-01-20 00:09:54 +0800

    feat: command `exec` amazing

---
+++ container/nsenter/nsenter.go
@@ -0,0 +1,48 @@
+package nsenter
+
+/*
+#include <errno.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+__attribute__((constructor)) void enter_namespace(void) {
+	char *dockerc_pid;
+	dockerc_pid = getenv("dockerc_pid");
+	if (dockerc_pid) {
+		//fprintf(stdout, "got dockerc_pid=%s\n", dockerc_pid);
+	} else {
+		//fprintf(stdout, "missing dockerc_pid env skip nsenter");
+		return;
+	}
+	char *dockerc_cmd;
+	dockerc_cmd = getenv("dockerc_cmd");
+	if (dockerc_cmd) {
+		//fprintf(stdout, "got dockerc_cmd=%s\n", dockerc_cmd);
+	} else {
+		//fprintf(stdout, "missing dockerc_cmd env skip nsenter");
+		return;
+	}
+	int i;
+	char nspath[1024];
+	char *namespaces[] = { "ipc", "uts", "net", "pid", "mnt" };
+
+	for (i=0; i<5; i++) {
+		sprintf(nspath, "/proc/%s/ns/%s", dockerc_pid, namespaces[i]);
+		int fd = open(nspath, O_RDONLY);
+
+		if (setns(fd, 0) == -1) {
+			//fprintf(stderr, "setns on %s namespace failed: %s\n", namespaces[i], strerror(errno));
+		} else {
+			//fprintf(stdout, "setns on %s namespace succeeded\n", namespaces[i]);
+		}
+		close(fd);
+	}
+	int res = system(dockerc_cmd);
+	exit(0);
+	return;
+}
+*/
+import "C"
---
+++ exec.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/exfly/dockerc/container"
+	_ "github.com/exfly/dockerc/container/nsenter"
+)
+
+const ENV_EXEC_PID = "dockerc_pid"
+const ENV_EXEC_CMD = "dockerc_cmd"
+
+func ExecContainer(containerName string, comArray []string) {
+	pid, err := getContainerPidByName(containerName)
+	if err != nil {
+		log.Errorf("Exec container getContainerPidByName %s error %v", containerName, err)
+		return
+	}
+	cmdStr := strings.Join(comArray, " ")
+	log.Infof("container pid %s", pid)
+	log.Infof("command %s", cmdStr)
+
+	cmd := exec.Command("/proc/self/exe", "exec")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	os.Setenv(ENV_EXEC_PID, pid)
+	os.Setenv(ENV_EXEC_CMD, cmdStr)
+
+	if err := cmd.Run(); err != nil {
+		log.Errorf("Exec container %s error %v", containerName, err)
+	}
+}
+
+func getContainerPidByName(containerName string) (string, error) {
+	dirURL := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	configFilePath := dirURL + container.ConfigName
+	contentBytes, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		return "", err
+	}
+	var containerInfo container.ContainerInfo
+	if err := json.Unmarshal(contentBytes, &containerInfo); err != nil {
+		return "", err
+	}
+	return containerInfo.Pid, nil
+}
