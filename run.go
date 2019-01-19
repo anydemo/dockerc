550ba053907476812f75b4fa24bf94aac337d2a4
haifei.zhang <haifei.zhang@chaitin.com>
2019-01-19 16:12:26 +0800

    add some cgroups utils && add pipeline support

--- run.go
+++ run.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/exfly/dockerc/container"
 )
 
-func Run(tty bool, command string) {
-	parent := container.NewParentProcess(tty, command)
+func Run(tty bool, comArray []string) {
+	parent, writePipe := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New parent process error")
+		return
+	}
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
+	sendInitCommand(comArray, writePipe)
 	parent.Wait()
-	os.Exit(-1)
+	os.Exit(0)
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("command all is %s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
 }
